3eb1344ad66fce7ee86b120130e7e40fb1f6e3d1
p-tecza <pawel.tecza.pt@gmail.com>
2024-01-20 17:19:08 +0100

    tdcc

---
+++ Assets/TestScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerObjectRef;
+
+    [SerializeField]
+    private Transform enemyTransformComponentRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
